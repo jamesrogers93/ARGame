0bc5d8949e6d3f8cb2f55e795edd809afee3051d
James Rogers <jamesrogers93@hotmail.co.uk>
2017-04-06 18:03:13 +0100

    Added a new animation file to the project

---
+++ ARGame/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  ARGame
+//
+//  Created by James Rogers on 06/04/2017.
+//  Copyright © 2017 James Rogers. All rights reserved.
+//
+
+import Foundation
+
+class Model
+{
+    
+}
